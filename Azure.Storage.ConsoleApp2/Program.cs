6c8dcdaa18f9edf9c1db1e3bc0d6fb03b661a7ec
acf0x <alcasfer96@gmail.com>
2024-06-21 12:03:08 +0200

    finished ConsoleApp2

--- Azure.Storage.ConsoleApp2/Program.cs
+++ Azure.Storage.ConsoleApp2/Program.cs
@@ -4,9 +4,32 @@ namespace Azure.Storage.ConsoleApp2
 {
     internal class Program
     {
+        static string connectionString = "DefaultEndpointsProtocol=https;AccountName=demostorageacf;AccountKey=/4jCzw5CpzsNul9shtuYuk+9DRQPyAB4AdygkKMlvX7QgT1kV3xpAs6TuFfjTh5sycHU57xgSBF9+AStFxb+dg==;EndpointSuffix=core.windows.net";
+        static BlobServiceClient client;
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Configurar el cliente
+            client = new BlobServiceClient(connectionString);
+
+            // Listado de contenedores
+            var contenedores = client.GetBlobContainers();
+            foreach (var container in contenedores)
+            {
+                Console.WriteLine($"Nombre del contenedor: {container.Name}");
+
+                // Listado de Blobs
+                var containerClient = client.GetBlobContainerClient(container.Name);
+                var blobs = containerClient.GetBlobs();
+
+                foreach (var blob in blobs)
+                {
+                    Console.WriteLine($"  > {blob.Name} ({blob.Properties.ContentType})");
+
+                    var blobClient = containerClient.GetBlobClient(blob.Name);
+                    blobClient.DownloadTo(@$"C:\EOIFormación\azure-csharp-main\Azure.Storage.ConsoleApp2\{blob.Name}");
+                }
+                Console.WriteLine($"");
+            }
         }
     }
 }
