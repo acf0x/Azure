2ffb6bb56b453302e53d375e75b25e8ad7232611
acf0x <alcasfer96@gmail.com>
2024-06-21 10:48:37 +0200

    finished ConsoleApp1 (getting blobs) and start ConsoleApp2 (same but with Azure.Storage.Blobs dependency)

--- Azure.Storage.ConsoleApp1/Program.cs
+++ Azure.Storage.ConsoleApp1/Program.cs
@@ -1,5 +1,4 @@
 ﻿using StorageRestApiAuth;
-using System.ComponentModel;
 using System.Globalization;
 using System.Xml.Linq;
 
@@ -7,65 +6,170 @@ namespace Azure.Storage.ConsoleApp1
 {
     internal class Program
     {
-        static string storageName = "";
-        static string storageKey = "";
-
-
+        static string storageName = "demostorageacf";
+        static string storageKey = @"/4jCzw5CpzsNul9shtuYuk+9DRQPyAB4AdygkKMlvX7QgT1kV3xpAs6TuFfjTh5sycHU57xgSBF9+AStFxb+dg==";
 
         static void Main(string[] args)
         {
-
+            Console.Clear();
+            GetListContainers();
         }
 
         static void GetListContainers()
         {
             HttpClient http = new HttpClient();
             string url = $"https://{storageName}.blob.core.windows.net/?comp=list";
+
             //string url = $"https://{storageName}.blob.core.windows.net/?comp=list&include=system,deleted";
 
+            ////////////////////////////////////////////////////////////////////
             // Mensaje de petición al API
+            ////////////////////////////////////////////////////////////////////
             HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, url);
             request.Content = null;
 
             DateTime fecha = DateTime.UtcNow;
 
             request.Headers.Add("x-ms-date", fecha.ToString("R", CultureInfo.InvariantCulture));
-            request.Headers.Add("x-ms-version", "2021-12-02");
-            request.Headers.Authorization = AzureStorageAuthenticationHelper.GetAuthorizationHeader(storageName, storageKey, fecha, request);
+            request.Headers.Add("x-ms-version", "2024-08-04");
+            request.Headers.Authorization = AzureStorageAuthenticationHelper
+                .GetAuthorizationHeader(storageName, storageKey, fecha, request);
 
-            // Mensaje de respuesta
+            ////////////////////////////////////////////////////////////////////
+            // Mensaje de respuesta del API
+            ////////////////////////////////////////////////////////////////////
             HttpResponseMessage response = http.Send(request);
 
             if (response.StatusCode == System.Net.HttpStatusCode.OK)
             {
                 string responseData = response.Content.ReadAsStringAsync().Result;
                 XElement xml = XElement.Parse(responseData);
-                foreach(var element in xml.Element("Containers").Elements("Container"))
+                foreach (var element in xml.Element("Containers").Elements("Container"))
                 {
-                    Console.WriteLine($"Nombre del Contenedor: {element.Element("name").Value}");
+                    Console.WriteLine($"Nombre del Contenedor: {element.Element("Name").Value}");
+                    GetListBlobs(element.Element("Name").Value);
+                }
 
-                    // ESTRUCTURA INTERNA:
-                    // ==========================================
-                    //  < containers >
+                // ESTRUCTURA INTERNA:
+                // ==========================================
+                //  < containers >
 
-                    //      < container >
-                    //          < name > publico < /name >
-                    //      < /container >
+                //      < container >
+                //          < name > publico < /name >
+                //      < /container >
 
-                    //       < container >
-                    //          < name > privado < /name >
-                    //      < /container >
+                //       < container >
+                //          < name > privado < /name >
+                //      < /container >
 
-                    //      < container >
-                    //          < name >$log < /name >
-                    //      < /container >
+                //      < container >
+                //          < name >$log < /name >
+                //      < /container >
 
-                    //  < /containers >
-                    // ==========================================
+                //  < /containers >
+                // ==========================================
+            }
 
+            else Console.WriteLine($"Error: {response.StatusCode}");
+        }
+
+        static void GetListBlobs(string containerName)
+        {
+            HttpClient http = new HttpClient();
+            string url = $"https://{storageName}.blob.core.windows.net/{containerName}?restype=container&comp=list";
+
+            //string url = $"https://{storageName}.blob.core.windows.net/?{containerName}?restype=container&comp=list&include=system,deleted";
+
+            ////////////////////////////////////////////////////////////////////
+            // Mensaje de petición al API
+            ////////////////////////////////////////////////////////////////////
+            HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, url);
+            request.Content = null;
+
+            DateTime fecha = DateTime.UtcNow;
+
+            request.Headers.Add("x-ms-date", fecha.ToString("R", CultureInfo.InvariantCulture));
+            request.Headers.Add("x-ms-version", "2024-08-04");
+            request.Headers.Authorization = AzureStorageAuthenticationHelper
+                .GetAuthorizationHeader(storageName, storageKey, fecha, request);
+
+            ////////////////////////////////////////////////////////////////////
+            // Mensaje de respuesta del API
+            ////////////////////////////////////////////////////////////////////
+            HttpResponseMessage response = http.Send(request);
+
+            if (response.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                string responseData = response.Content.ReadAsStringAsync().Result;
+                XElement xml = XElement.Parse(responseData);
+                foreach (var element in xml.Element("Blobs").Elements("Blob"))
+                {
+                    Console.WriteLine($"  > {element.Element("Name").Value}");
+                    if (containerName == "publico") GetBlob(containerName, element.Element("Name").Value);
+                }
+                Console.WriteLine("========================================");
+            }
+
+            else Console.WriteLine($"Error: {response.StatusCode}");
+        }
+
+        static void GetBlob(string containerName, string blobName)
+        {
+            HttpClient http = new HttpClient();
+            string url = $"https://{storageName}.blob.core.windows.net/{containerName}/{blobName}";
+
+            //string url = $"https://{storageName}.blob.core.windows.net/?comp=list&include=system,deleted";
+
+            ////////////////////////////////////////////////////////////////////
+            // Mensaje de petición al API
+            ////////////////////////////////////////////////////////////////////
+            HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, url);
+            request.Content = null;
+
+            DateTime fecha = DateTime.UtcNow;
+
+            request.Headers.Add("x-ms-date", fecha.ToString("R", CultureInfo.InvariantCulture));
+            request.Headers.Add("x-ms-version", "2024-08-04");
+            request.Headers.Authorization = AzureStorageAuthenticationHelper
+                .GetAuthorizationHeader(storageName, storageKey, fecha, request);
+
+            ////////////////////////////////////////////////////////////////////
+            // Mensaje de respuesta del API
+            ////////////////////////////////////////////////////////////////////
+            HttpResponseMessage response = http.Send(request);
+
+            if (response.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                Console.WriteLine($"    - Content-Type: {response.Content.Headers.ContentType.MediaType}");
+                switch (response.Content.Headers.ContentType.MediaType.ToString().ToLower())
+                {
+                    case "text/plain":
+                        string contenido = response.Content.ReadAsStringAsync().Result;
+                        Console.WriteLine(contenido);
+
+                        // Opción 1
+                        StreamWriter writer = new StreamWriter(@$"C:\EOIFormación\azure-csharp-main\Azure.Storage.ConsoleApp1\{blobName}");
+                        writer.Write(contenido);
+                        writer.Close();
+                        writer.Dispose();
+
+                        // Opción 2
+                        File.WriteAllText(@$"C:\EOIFormación\azure-csharp-main\Azure.Storage.ConsoleApp1\2_{blobName}", contenido);
+
+                        break;
+
+                    default:
+                        var file = new FileStream(@$"C:\EOIFormación\azure-csharp-main\Azure.Storage.ConsoleApp1\{blobName}", FileMode.Create, FileAccess.Write);
+                        response.Content.ReadAsStream().CopyTo(file);
+                        file.Close();
+                        file.Dispose();
+                        break;
                 }
             }
+
             else Console.WriteLine($"Error: {response.StatusCode}");
         }
     }
 }
+// Otras operaciones del API:
+// https://learn.microsoft.com/es-es/rest/api/storageservices/blob-service-rest-api
---
+++ Azure.Storage.ConsoleApp2/Program.cs
@@ -0,0 +1,12 @@
+﻿using Azure.Storage.Blobs;
+
+namespace Azure.Storage.ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
